a8262ce39519c9971ff15d1fd17afea3c9931602
Facundo Olano <facundo.olano@gmail.com>
2017-07-05 10:39:01 -0300

    move request to a separate module

--- src/channels/slack_channel.erl
+++ src/channels/slack_channel.erl
@@ -6,13 +6,11 @@ handle(Config, Message) ->
     %% FIXME Config keys should be atoms, not binaries, fix hp_json decode
     Payload = #{
       channel => maps:get(<<"channel">>, Config),
-      %% need to make it binary for proper json encoding, if all channels require this just do it on message building
       text => Message,
       username => maps:get(<<"username">>, Config, <<"Holiday Reminder">>),
       icon_emoji => maps:get(<<"emoji">>, Config, <<":calendar:">>)
      },
 
-    JsonPayload = hp_json:encode(Payload),
     HookUrl = maps:get(<<"url">>, Config),
     lager:debug("Sending request to slack: ~p", [Payload]),
-    hackney:post(HookUrl, [{<<"Content-Type">>, <<"application/json">>}], JsonPayload, []).
+    hp_request:post_json(HookUrl, Payload).
