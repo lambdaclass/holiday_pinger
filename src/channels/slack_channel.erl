201062ab1fc3c904e91dacf6f87ec8fed9ada8f5
Facundo Olano <facundo.olano@gmail.com>
2017-07-17 11:20:20 -0300

    support multiple targets in the slack channel, fixes #39

--- src/channels/slack_channel.erl
+++ src/channels/slack_channel.erl
@@ -3,13 +3,17 @@
 -export([handle/2]).
 
 handle(Config, Message) ->
-    Payload = #{
-      channel => maps:get(channel, Config),
+    Targets = maps:get(channels, Config),
+    HookUrl = maps:get(url, Config),
+    BasePayload = #{
       text => Message,
       username => maps:get(username, Config, <<"Holiday Reminder">>),
       icon_emoji => maps:get(emoji, Config, <<":calendar:">>)
      },
 
-    HookUrl = maps:get(url, Config),
-    lager:debug("Sending request to slack: ~p", [Payload]),
-    {ok, 200, _, _} = hp_request:post_json(HookUrl, Payload).
+    lists:foreach(
+      fun (Target) ->
+              Payload = BasePayload#{channel => Target},
+              lager:debug("Sending request to slack: ~p", [Payload]),
+              {ok, 200, _, _} = hp_request:post_json(HookUrl, Payload)
+      end, Targets).
