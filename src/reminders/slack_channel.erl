68be92b7c237e4e196c9e8bda4d05296501816dc
Facundo Olano <facundo.olano@gmail.com>
2017-09-21 15:04:01 -0300

    optional slack targets, fixes #125

--- src/reminders/slack_channel.erl
+++ src/reminders/slack_channel.erl
@@ -12,12 +12,21 @@ handle(Config, Message) ->
     icon_emoji => get_not_empty(emoji, Config, <<":calendar:">>)
    },
 
+  send_to_slack(HookUrl, BasePayload, Targets).
+
+%%% internal
+send_to_slack(HookUrl, Payload, Targets) when is_list(Targets), length(Targets) > 0 ->
   lists:foreach(
-    fun (Target) ->
-        Payload = BasePayload#{channel => Target},
-        lager:debug("Sending request to slack: ~p", [Payload]),
-        {ok, 200, _, _} = hp_request:post_json(HookUrl, Payload)
-    end, Targets).
+    fun (Target) -> do_send(HookUrl, Payload#{channel => Target}) end,
+    Targets
+   );
+send_to_slack(HookUrl, Payload, _) ->
+  %% don't set the channel, use configured default
+  do_send(HookUrl, Payload).
+
+do_send(HookUrl, Payload) ->
+  lager:debug("Sending request to slack: ~p", [Payload]),
+  {ok, 200, _, _} = hp_request:post_json(HookUrl, Payload).
 
 get_not_empty(Key, Map, Default) ->
   Value = maps:get(Key, Map, Default),
