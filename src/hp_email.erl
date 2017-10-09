6ac3d3e478154869359641db854548a27e602764
Facundo Olano <facundo.olano@gmail.com>
2017-10-09 18:07:05 -0300

    add footer to emails, fixes #148

--- src/hp_email.erl
+++ src/hp_email.erl
@@ -1,42 +1,60 @@
 -module(hp_email).
 
 -export([send/4,
+         send_html/4,
          send_email_verification/2,
          send_password_reset/2]).
 
 send(ToEmails, FromEmail, Subject, Body) when not is_list(ToEmails)->
   send([ToEmails], FromEmail, Subject, Body);
 send(ToEmails, FromEmail, Subject, Body) ->
-  lager:debug(<<"Sending amazon SES emails to ~p">>, [ToEmails]),
+  lager:debug(<<"Sending amazon SES emails: ~p ~p">>, [Subject, ToEmails]),
+  FullBody = add_footer(Body),
 
   case hp_config:get(email_enabled) of
     true ->
       lists:foreach(fun(Email) ->
-                        erlcloud_ses:send_email(Email, Body, Subject, FromEmail, [])
+                        erlcloud_ses:send_email(Email, FullBody, Subject, FromEmail, [])
                     end, ToEmails);
     _ ->
       ok
   end.
 
+send_html(ToEmails, FromEmail, Subject, Body) ->
+  send(ToEmails, FromEmail, Subject, [{html, Body}]).
+
 send_email_verification(Email, Code) ->
   From = <<"HolidayPing <holidayping@lambdaclass.com>">>,
   Subject = <<"HolidayPing email confirmation">>,
-  Body = <<"Hey there!\n\n"
-           "Thanks for choosing at HolidayPing, "
-           "please click on the link below to finish the registration process:\n\n"
-           "https://holidayping.lambdaclass.com/register/confirm/code?code=", Code/binary,
-           "&email=", Email/binary,
-           "\n\nThanks,\nThe HolidayPing team.">>,
+  Link = <<"https://holidayping.lambdaclass.com/register/confirm/code?code=", Code/binary,
+           "&email=", Email/binary>>,
+  Body = <<"<p>Hey there!</p>"
+           "<p>Thanks for choosing HolidayPing, "
+           "Please click on <a href=\"", Link/binary, "\">this link</a>"
+           " to finish the registration process.</p>"
+           "<p>The HolidayPing team</p">>,
 
-  send(Email, From, Subject, Body).
+  send_html(Email, From, Subject, Body).
 
 send_password_reset(Email, Code) ->
   From = <<"HolidayPing <holidayping@lambdaclass.com>">>,
   Subject = <<"HolidayPing password reset">>,
-  Body = <<"Hey there!\n\n"
-           "Please click on the link below to reset your password:\n\n"
-           "https://holidayping.lambdaclass.com/password/code?code=", Code/binary,
-           "&email=", Email/binary,
-           "\n\nThanks,\nThe HolidayPing team.">>,
+  Link = <<"https://holidayping.lambdaclass.com/password/code?code=", Code/binary,
+           "&email=", Email/binary>>,
+  Body = <<"<p>Hey there!</p>"
+           "Please click on <a href=\"", Link/binary, "\">this link</a>"
+           " to reset your password.</p>"
+           "<p>The HolidayPing team</p">>,
 
-  send(Email, From, Subject, Body).
+  send_html(Email, From, Subject, Body).
+
+%%% internal
+
+add_footer([{html, Body}]) ->
+  HolidayLink = <<"<a href=\"https://holidayping.lambdaclass.com\">HolidayPing</a>">>,
+  AbuseLink = <<"<a href=\"mailto:holidayping@lambdaclass.com\">Report abuse</a>">>,
+  [{html, <<Body/binary,
+            "<p><br/><small>Mail sent by ", HolidayLink/binary,
+            " | ", AbuseLink/binary, "</small></p>">>}];
+add_footer(Body) ->
+  add_footer([{html, <<"<p>", Body/binary, "</p>">>}]).
