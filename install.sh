2078afa7e75a2c34307eac8b97316e91fa3e6c36
unbalancedparentheses <federico.carrone@gmail.com>
2017-11-14 21:36:04 +0000

    Add install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y wget curl git tmux postgresql build-essential libssl-dev make automake autoconf libncurses5-dev gcc default-jre certbot
+apt-get install -y wget curl git tmux postgresql build-essential libssl-dev make automake autoconf libncurses5-dev gcc default-jre certbot nginx ufw
 
 # install erlang
 curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
@@ -9,6 +9,7 @@ kerl build 20.1 20.1
 mkdir -p /srv/otp/20.1/
 kerl install 20.1 /srv/otp/20.1/
 source /srv/otp/20.1/activate
+echo "source /srv/otp/20.1/activate" >> .bashrc
 
 # add leningen
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
@@ -25,16 +26,28 @@ sudo -u postgres psql -c "ALTER USER holiday_ping WITH PASSWORD 'holiday_ping';"
 cp /root/holiday_ping/priv/sql/tables.sql /tmp/
 sudo -u postgres psql -a -f /tmp/tables.sql
 
-# get ssl certificates
+# get ssl certificates. issue with already running port
 certbot certonly --standalone -d holidayping.lambdaclass.com
 
-# TODO add sed to modiy pems and user, passwords and other things in config
-#/etc/letsencrypt/live/holidayping.lambdaclass.com/privkey.pem
-#/etc/letsencrypt/live/holidayping.lambdaclass.com/fullchain.pem
+#add nginx
+echo 'server {
+       listen         80;
+       server_name    holidayping.lambdaclass.com;
+       return         301 https://$server_name$request_uri;
+}' > /etc/nginx/sites-available/default
 
 #generate release
 make release
 
 # run release
-_build/prod/rel/holiday_ping/bin/holiday_ping start
-_build/prod/rel/holiday_ping/bin/holiday_ping attach
+/root/holiday_ping/_build/prod/rel/holiday_ping/bin/holiday_ping start
+/root/holiday_ping/_build/prod/rel/holiday_ping/bin/holiday_ping attach
+
+# add ufw
+sed -i -e "s/DEFAULT_INPUT_POLICY=\"DROP\"/DEFAULT_INPUT_POLICY=\"ACCEPT\"/" /etc/default/ufw
+awk '!found && /COMMIT/ { print "-A ufw-reject-input -j DROP"; found=1 } 1' /etc/ufw/after.rules > /tmp/after.rules && mv /tmp/after.rules /etc/ufw/after.rules
+
+ufw allow ssh
+ufw allow https
+ufw allow http
+ufw enable
